d5d6170bd607d14ae9d49075acf8ce58f5bae597
ghongli <18810027593@163.com>
2023-11-13 23:41:35 +0800

    Interpreter Pattern to split complex func, e.g. defined add,min,val

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -2,6 +2,7 @@ package interpreter
 
 import (
 	"fmt"
+	"testing"
 )
 
 func ExampleInterpret() {
@@ -13,4 +14,14 @@ func ExampleInterpret() {
 	fmt.Println(i.Interpret())
 	// Output:
 	// true
+}
+
+func TestInterpreter(t *testing.T) {
+	p := &Parser{}
+	p.Parse("1 + 2 + 3 - 4 + 5 - 6")
+	got := p.Result().Interpret()
+	expected := 1
+	if got != expected {
+		t.Fatalf("expected %d, but got %d", expected, got)
+	}
 }
